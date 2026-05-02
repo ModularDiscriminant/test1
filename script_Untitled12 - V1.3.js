13cd55a274e2562a7920f85054a702e0f64bf237
ModularDiscriminant <ModularDiscriminant@gmail.com>
2026-05-02 21:36:17 +0900

    Untitled12 - V1.3.html 문서, style_Untitled12 - V1.3.css 파일, script_Untitled12 - V1.3.js 프로그램을 만듦

---
+++ script_Untitled12 - V1.3.js
@@ -0,0 +1,21 @@
+//2026/5/2
+//(이 문서 만든 시각: 2026/5/2 19:55:36)
+
+
+
+const PageNumInput = document.getElementById("PageNum"); //(2026/5/2 20:59:59)
+
+function ChangePage()
+{
+
+}
+
+function PrevPage()
+{
+
+}
+
+function NextPage()
+{
+
+}
